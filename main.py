c9889e7018a766301b6884c513e8edaea97b63bd
padmanabhan08 <padmanabhan123345@gmail.com>
2025-09-18 09:26:54 +0530

    verbose

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import os
+from dotenv import load_dotenv
+import sys
+from google import genai
+from google.genai import types
 def main():
-    print("Hello from agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
 
 
-if __name__ == "__main__":
-    main()
+    if len(sys.argv)<2:
+        print("need a prompt")
+        sys.exit(1)
+    verbose_flag=False
+    if len(sys.argv)==3 and sys.argv[2]=='--verbose':
+        verbose_flag=True
+
+
+    prompt=sys.argv[1]
+    messages=[types.Content(role="user",parts=[types.Part(text=prompt)])]
+
+
+    response=client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
+
+    print(response.text)
+    if verbose_flag:
+        print(response.usage_metadata.prompt_token_count)
+        print(response.usage_metadata.candidates_token_count)
+
+
+main()
